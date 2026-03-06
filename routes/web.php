d4bb4876b8f507d7cd86e5c8b096387bdc878eb7
sujiwo22 <massujiwo2288@gmail.com>
2026-03-06 22:39:28 +0700

    Add middleware in route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserLevelController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Models\UserLevel;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,16 +11,16 @@ Route::get('/', function () {
 // Route::get('/login', [LoginController::class, 'index'])->name('home');
 
 Auth::routes();
-// Route::middleware('auth')->group(function () {
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// Route::resource('/user_level', UserLevelController::class);
+    // Route::resource('/user_level', UserLevelController::class);
 
-Route::get('/user_level', [UserLevelController::class, 'index'])->name('user_level');
-Route::post('/user_level', [UserLevelController::class, 'store'])->name('user_level.store');
-// Route::post('/user_level', [UserLevelController::class, 'update'])->name('user_level.update');
-// Route::get('/user_level/edit/{id}', [UserLevelController::class, 'edit'])->name('user_level.edit');
-Route::get('/user_level/{id}', [UserLevelController::class, 'show'])->name('user_level.show');
-Route::delete('/user_level/{id}', [UserLevelController::class, 'destroy'])->name('user_level.destroy');
+    Route::get('/user_level', [UserLevelController::class, 'index'])->name('user_level');
+    Route::post('/user_level', [UserLevelController::class, 'store'])->name('user_level.store');
+    // Route::post('/user_level', [UserLevelController::class, 'update'])->name('user_level.update');
+    // Route::get('/user_level/edit/{id}', [UserLevelController::class, 'edit'])->name('user_level.edit');
+    Route::get('/user_level/{id}', [UserLevelController::class, 'show'])->name('user_level.show');
+    Route::delete('/user_level/{id}', [UserLevelController::class, 'destroy'])->name('user_level.destroy');
 
-// });
+});
