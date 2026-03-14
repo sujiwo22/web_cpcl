f36af39f6c9a90e3dfeb29f0d147b2da1f533f21
sujiwo22 <massujiwo2288@gmail.com>
2026-03-14 09:21:41 +0700

    Migration table kementrian

--- database/migrations/2026_03_07_070805_create_kementrians_table.php
+++ database/migrations/2026_03_07_070805_create_kementrians_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('kementrians', function (Blueprint $table) {
             $table->id();
             $table->string('nama_kementrian');
+            $table->string('singkatan');
             $table->integer('crt_id_user');
             $table->integer('upd_id_user')->nullable()->default(null);
             $table->integer('del_id_user')->nullable()->default(null);
